bcf01a362c49a64f5c456a246061792ba18e0bd3
Try2hardx <yenusam14@gmail.com>
2026-03-28 07:56:07 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.__DT_CFG = (function(){
+  var _a = 'https://docs.google.com/spreadsheets/d/e/';
+  var _b = '2PACX-1vQyVAQS4iWEr7Q1CXFfmWGiS5eLpE-IsrNXgVWZrjGYlSQ0Q0iFG4tXD2jP5RXcJjkft93AoMhrNEJp';
+  var _c = '/pub?output=csv';
+  return { csv: _a + _b + _c };
+})();
